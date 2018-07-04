002314309a62185f60d8da426a9fdb1bee8acd0d
Alper <alper_alacam@hotmail.com>
2018-07-04 16:50:00 +0300

    Setting first code

--- Spam-mer/Form1.Designer.vb
+++ Spam-mer/Form1.Designer.vb
@@ -22,64 +22,66 @@ Partial Class SpamMer
     'Kod düzenleyicisini kullanarak değiştirmeyin.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(SpamMer))
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.closebutton = New System.Windows.Forms.Button()
         Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.Panel2 = New System.Windows.Forms.Panel()
-        Me.Label1 = New System.Windows.Forms.Label()
         Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
-        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Button5 = New System.Windows.Forms.Button()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Button4 = New System.Windows.Forms.Button()
-        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
         Me.Button6 = New System.Windows.Forms.Button()
-        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.DomainUpDown1 = New System.Windows.Forms.DomainUpDown()
-        Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.Panel1.SuspendLayout()
         Me.Panel2.SuspendLayout()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'Button1
+        'closebutton
         '
-        Me.Button1.BackgroundImage = CType(resources.GetObject("Button1.BackgroundImage"), System.Drawing.Image)
-        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
-        Me.Button1.Cursor = System.Windows.Forms.Cursors.No
-        Me.Button1.FlatAppearance.BorderSize = 0
-        Me.Button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red
-        Me.Button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White
-        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.Button1.Location = New System.Drawing.Point(332, 0)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(42, 36)
-        Me.Button1.TabIndex = 0
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.closebutton.BackgroundImage = CType(resources.GetObject("closebutton.BackgroundImage"), System.Drawing.Image)
+        Me.closebutton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.closebutton.Cursor = System.Windows.Forms.Cursors.No
+        Me.closebutton.FlatAppearance.BorderSize = 0
+        Me.closebutton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red
+        Me.closebutton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White
+        Me.closebutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.closebutton.Location = New System.Drawing.Point(332, 0)
+        Me.closebutton.Name = "closebutton"
+        Me.closebutton.Size = New System.Drawing.Size(42, 36)
+        Me.closebutton.TabIndex = 0
+        Me.closebutton.UseVisualStyleBackColor = True
         '
         'Panel1
         '
         Me.Panel1.BackColor = System.Drawing.Color.DarkBlue
         Me.Panel1.Controls.Add(Me.LinkLabel1)
         Me.Panel1.Controls.Add(Me.Label1)
-        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.closebutton)
         Me.Panel1.Location = New System.Drawing.Point(0, 0)
         Me.Panel1.Name = "Panel1"
         Me.Panel1.Size = New System.Drawing.Size(377, 40)
         Me.Panel1.TabIndex = 1
         '
-        'Panel2
+        'LinkLabel1
         '
-        Me.Panel2.BackColor = System.Drawing.Color.DarkBlue
-        Me.Panel2.Controls.Add(Me.Button5)
-        Me.Panel2.Controls.Add(Me.Button3)
-        Me.Panel2.Controls.Add(Me.Button4)
-        Me.Panel2.Controls.Add(Me.Button2)
-        Me.Panel2.Location = New System.Drawing.Point(0, 238)
-        Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(377, 88)
-        Me.Panel2.TabIndex = 2
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.LinkColor = System.Drawing.Color.White
+        Me.LinkLabel1.Location = New System.Drawing.Point(197, 10)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(104, 26)
+        Me.LinkLabel1.TabIndex = 3
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "We dont responsible" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "for any abuse! "
         '
         'Label1
         '
@@ -93,32 +95,33 @@ Partial Class SpamMer
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "Spam-Mer"
         '
-        'LinkLabel1
+        'Panel2
         '
-        Me.LinkLabel1.AutoSize = True
-        Me.LinkLabel1.LinkColor = System.Drawing.Color.White
-        Me.LinkLabel1.Location = New System.Drawing.Point(204, 21)
-        Me.LinkLabel1.Name = "LinkLabel1"
-        Me.LinkLabel1.Size = New System.Drawing.Size(78, 13)
-        Me.LinkLabel1.TabIndex = 3
-        Me.LinkLabel1.TabStop = True
-        Me.LinkLabel1.Text = "communication"
+        Me.Panel2.BackColor = System.Drawing.Color.DarkBlue
+        Me.Panel2.Controls.Add(Me.Button5)
+        Me.Panel2.Controls.Add(Me.Button3)
+        Me.Panel2.Controls.Add(Me.Button4)
+        Me.Panel2.Controls.Add(Me.Button2)
+        Me.Panel2.Location = New System.Drawing.Point(0, 238)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(377, 88)
+        Me.Panel2.TabIndex = 2
         '
-        'Button2
+        'Button5
         '
-        Me.Button2.BackgroundImage = CType(resources.GetObject("Button2.BackgroundImage"), System.Drawing.Image)
-        Me.Button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
-        Me.Button2.FlatAppearance.BorderSize = 0
-        Me.Button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Yellow
-        Me.Button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Cyan
-        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.Button2.Location = New System.Drawing.Point(0, 0)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(93, 88)
-        Me.Button2.TabIndex = 0
-        Me.Button2.TabStop = False
-        Me.Button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.Button5.BackgroundImage = CType(resources.GetObject("Button5.BackgroundImage"), System.Drawing.Image)
+        Me.Button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.Button5.FlatAppearance.BorderSize = 0
+        Me.Button5.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Yellow
+        Me.Button5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Cyan
+        Me.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button5.Location = New System.Drawing.Point(284, 0)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(93, 88)
+        Me.Button5.TabIndex = 5
+        Me.Button5.TabStop = False
+        Me.Button5.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+        Me.Button5.UseVisualStyleBackColor = True
         '
         'Button3
         '
@@ -153,83 +156,89 @@ Partial Class SpamMer
         Me.Button4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
         Me.Button4.UseVisualStyleBackColor = True
         '
-        'Button5
+        'Button2
         '
-        Me.Button5.BackgroundImage = CType(resources.GetObject("Button5.BackgroundImage"), System.Drawing.Image)
-        Me.Button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
-        Me.Button5.FlatAppearance.BorderSize = 0
-        Me.Button5.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Yellow
-        Me.Button5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Cyan
-        Me.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.Button5.Location = New System.Drawing.Point(284, 0)
-        Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(93, 88)
-        Me.Button5.TabIndex = 5
-        Me.Button5.TabStop = False
-        Me.Button5.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
-        Me.Button5.UseVisualStyleBackColor = True
+        Me.Button2.BackgroundImage = CType(resources.GetObject("Button2.BackgroundImage"), System.Drawing.Image)
+        Me.Button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.Button2.FlatAppearance.BorderSize = 0
+        Me.Button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Yellow
+        Me.Button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Cyan
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Location = New System.Drawing.Point(0, 0)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(93, 88)
+        Me.Button2.TabIndex = 0
+        Me.Button2.TabStop = False
+        Me.Button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+        Me.Button2.UseVisualStyleBackColor = True
         '
         'Button6
         '
-        Me.Button6.Location = New System.Drawing.Point(90, 177)
+        Me.Button6.Location = New System.Drawing.Point(78, 177)
         Me.Button6.Name = "Button6"
         Me.Button6.Size = New System.Drawing.Size(204, 43)
         Me.Button6.TabIndex = 3
         Me.Button6.Text = "Start"
         Me.Button6.UseVisualStyleBackColor = True
         '
-        'RadioButton1
-        '
-        Me.RadioButton1.AutoSize = True
-        Me.RadioButton1.Location = New System.Drawing.Point(62, 83)
-        Me.RadioButton1.Name = "RadioButton1"
-        Me.RadioButton1.Size = New System.Drawing.Size(99, 17)
-        Me.RadioButton1.TabIndex = 4
-        Me.RadioButton1.TabStop = True
-        Me.RadioButton1.Text = "Operation PID's"
-        Me.RadioButton1.UseVisualStyleBackColor = True
-        '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(172, 82)
+        Me.TextBox1.Enabled = False
+        Me.TextBox1.Location = New System.Drawing.Point(120, 53)
+        Me.TextBox1.MaxLength = 7
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(82, 20)
         Me.TextBox1.TabIndex = 5
         '
-        'DomainUpDown1
-        '
-        Me.DomainUpDown1.Location = New System.Drawing.Point(172, 135)
-        Me.DomainUpDown1.Name = "DomainUpDown1"
-        Me.DomainUpDown1.Size = New System.Drawing.Size(82, 20)
-        Me.DomainUpDown1.TabIndex = 6
-        Me.DomainUpDown1.Text = "1"
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(260, 129)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(101, 26)
-        Me.Label2.TabIndex = 7
-        Me.Label2.Text = "1 is selected " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "           automatically"
-        '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(260, 85)
+        Me.Label3.Location = New System.Drawing.Point(208, 53)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(44, 13)
+        Me.Label3.Size = New System.Drawing.Size(142, 26)
         Me.Label3.TabIndex = 8
-        Me.Label3.Text = "optional"
+        Me.Label3.Text = "You can find in taskmanager" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "(optional)"
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(59, 135)
+        Me.Label4.Location = New System.Drawing.Point(75, 104)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(105, 13)
+        Me.Label4.Size = New System.Drawing.Size(94, 13)
         Me.Label4.TabIndex = 9
-        Me.Label4.Text = "burayı bulamadım KB"
+        Me.Label4.Text = "Invertal seconds ="
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(12, 55)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(102, 17)
+        Me.CheckBox1.TabIndex = 10
+        Me.CheckBox1.Text = "Operation PID ="
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(177, 102)
+        Me.NumericUpDown1.Maximum = New Decimal(New Integer() {200, 0, 0, 0})
+        Me.NumericUpDown1.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(62, 20)
+        Me.NumericUpDown1.TabIndex = 11
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {15, 0, 0, 0})
+        '
+        'Timer1
+        '
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(75, 149)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(185, 13)
+        Me.Label2.TabIndex = 12
+        Me.Label2.Text = "Send random messages to active app"
         '
         'SpamMer
         '
@@ -238,12 +247,12 @@ Partial Class SpamMer
         Me.BackColor = System.Drawing.Color.DodgerBlue
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
         Me.ClientSize = New System.Drawing.Size(378, 326)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.CheckBox1)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.DomainUpDown1)
         Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.RadioButton1)
         Me.Controls.Add(Me.Button6)
         Me.Controls.Add(Me.Panel2)
         Me.Controls.Add(Me.Panel1)
@@ -254,12 +263,13 @@ Partial Class SpamMer
         Me.Panel1.ResumeLayout(False)
         Me.Panel1.PerformLayout()
         Me.Panel2.ResumeLayout(False)
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents Button1 As Button
+    Friend WithEvents closebutton As Button
     Friend WithEvents Panel1 As Panel
     Friend WithEvents LinkLabel1 As LinkLabel
     Friend WithEvents Label1 As Label
@@ -269,10 +279,11 @@ Partial Class SpamMer
     Friend WithEvents Button5 As Button
     Friend WithEvents Button4 As Button
     Friend WithEvents Button6 As Button
-    Friend WithEvents RadioButton1 As RadioButton
     Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents DomainUpDown1 As DomainUpDown
-    Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label2 As Label
 End Class
--- Spam-mer/Form1.vb
+++ Spam-mer/Form1.vb
@@ -1,13 +1,116 @@
-﻿Public Class SpamMer
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+﻿
+
+
+'This project made by DOSTeam
+'Maybe some english word incorrect
+
+
+
+Public Class SpamMer
+
+    Public running As Boolean = False
+    Public pidextising As Boolean = False
+    Public pid As Integer
+
+    Private Sub closebutton_Click(sender As Object, e As EventArgs) Handles closebutton.Click
+
+        'closing app
+        Timer1.Stop()
         Me.Close()
-    End Sub
-
-    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
 
     End Sub
 
-    Private Sub SpamMer_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+
+        If running = False Then
+
+            'start
+
+            'locking controls
+            Button6.Text = "stop"
+            CheckBox1.Enabled = False
+            TextBox1.Enabled = False
+            NumericUpDown1.Enabled = False
+
+            'checking PID
+
+            If CheckBox1.Checked = True Then
+                If TextBox1.Text = "" Then
+                    GoTo stp
+                Else
+                    pidextising = True
+                    pid = TextBox1.Text
+                End If
+            End If
+
+
+            'setting invertal seconds
+            Timer1.Interval = NumericUpDown1.Value * 1000
+
+
+
+            'start
+            Timer1.Start()
+
+
+            running = True
+
+
+
+        Else
+
+stp:
+            'stop
+
+            'unlocking controls
+
+            Button6.Text = "Start"
+            CheckBox1.Enabled = True
+            If CheckBox1.Checked = True Then TextBox1.Enabled = True
+            NumericUpDown1.Enabled = True
+
+            'stopping timer
+            Timer1.Stop()
+
+            running = False
+
+        End If
+
+
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+
+        'PID setting
+
+        If CheckBox1.Checked = True Then
+            TextBox1.Enabled = True
+        Else
+            TextBox1.Enabled = False
+        End If
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        'randomizing
+
+        Dim k As String() = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "y", "z"}
+
+        For i As Integer = 0 To 7
+            Randomize()
+            If pidextising = True Then AppActivate(pid)
+            SendKeys.Send((Rnd() * 22))
+        Next
+
+    End Sub
+
+    Private Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
+        If Not (Char.IsNumber(e.KeyChar) = True) And e.KeyChar <> ChrW(Keys.Back) Then
+            e.Handled = True
+        End If
     End Sub
 End Class
